45d8ce9b9c487fcd75d81ed1af8fff03df0154bd
Toshiki Yanagimoto <toshiyana0306@gmail.com>
2021-09-17 15:13:11 +0900

    [add] files

--- main.py
+++ main.py
@@ -45,5 +45,5 @@ def handle_message(event):
     )
 
 if __name__ == "__main__":
-    port = int(os.getenv("PORT", 5000))
+    port = int(os.getenv("PORT", 200))
     app.run(host="0.0.0.0", port=port)
