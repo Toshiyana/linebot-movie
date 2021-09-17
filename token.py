a4f44d651297f1581c05e4de542bde53e75a578c
Toshiki Yanagimoto <toshiyana0306@gmail.com>
2021-09-17 16:44:16 +0900

    [add] files

--- token.py
+++
@@ -1,4 +0,0 @@
-import os
-
-os.environ["LINE_CHANNEL_TOKEN"] = "fQ8b77NybQ+EtfxLh6iD1/E8CJCS0Fd65W74JvjIn0PTOvJtBkZGc3KEZ4TFk5PcgeEbAup+cwaWgaoahZPi7ond90M1yXT6aofkwv4NJsaFhlbassOFhcm647F3Q+0OTjTvhLF+j5GthqAbbt5lCQdB04t89/1O/w1cDnyilFU="
-os.environ["LINE_CHANNEL_SECRET"] = "0d9e856969ec1d9adc1dd934f97ba0d8"
